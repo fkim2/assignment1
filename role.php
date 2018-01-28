6fcb2e81283391a6c038b94e2d6f497b2a73dc22
fkim2 <35535550+fkim2@users.noreply.github.com>
2018-01-28 01:03:23 -0500

    Delete role.php

--- role.php
+++
@@ -1,3 +0,0 @@
-<?php echo "Initial Team Role in CSC 174 - Information Architect <br> The responsibilities of an information architect are to: prepare the content and its page organiziation, navigation design for browser type and size, and lead team collaboration on Github.";
-
-?>
