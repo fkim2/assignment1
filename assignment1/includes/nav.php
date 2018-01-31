4d96b7762502e604757f3bcd1a38fa99f325765c
fkim2 <35535550+fkim2@users.noreply.github.com>
2018-01-31 12:22:50 -0500

    Add files via upload

---
+++ assignment1/includes/nav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset= "utf-8">
+	<title> Fayth Kim </title>
+	<link rel = "stylesheet" href = "../css/style.css" />
+</head>
+<?php
+	$home= "button";
+	$introduction = "button";
+	$role = "button";
+	$activePage= basename($_SERVER['PHP_SELF']);
+
+	if($activePage=="index.php"){
+		$home = 'activeButton';
+	} else if($activePage=="introduction.php"){
+		$introduction = 'activeButton';
+	}else if($activePage =="role.php"){
+		$role = 'activeButton';
+	}
+?>
+
+<a class = "<?php echo $home;?>" href = "index.php">Home</a>
+<a class = "<?php echo $introduction;?>" href = "introduction.php">Introduction</a>
+<a class = "<?php echo $role;?>" href = "role.php">Initial Team Role</a>
+</html>
+
+
---
+++ assignment1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset= "utf-8">
+	<title> Fayth Kim </title>
+	<link rel = "stylesheet" href = "css/style.css" />
+</head>
+<body>
+<header>
+<h1> Fayth Kim </h1>
+<div id = "navContainer">
+	<?php include('includes/nav.php');?>
+</div>
+</header>
+<div id = "bodyContainer">
+	<img src="images/pic.jpg" alt = "Picture of Me">
+	
+		<p> Welcome to my initial portfolio website! The website is still under construction and will get better over time!</p>
+</div>
+</body>
+</html>
+
